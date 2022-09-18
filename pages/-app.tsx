cfd318b94e0a501648845bf383867b21a1688255
jesse uzoma <71229278+officailesse@users.noreply.github.com>
2022-09-17 19:17:05 -0600

    [feat]: added config file for app.tsx

---
+++ pages/-app.tsx
@@ -0,0 +1,14 @@
+import *as React from 'react'
+import type { AppProps } from 'next/app'
+import '../theme/fonts.css'
+import '../theme/globalStyles.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+    React.useEffect(() = {
+        let vh= window.innerHeight * 0.1,
+        document.documentElement.style.setProperty('--vh', `${vh}px`)
+    }, [])
+    return <Component {...pageProps} />
+}
+
+export default MyApp
